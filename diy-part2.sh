491113217de8826ee762c80becd477c84cba050b
yeungle <75871729+yeungle@users.noreply.github.com>
2026-06-04 09:25:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,25 +19,25 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-adguardhome
-rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/luci/applications/luci-app-openclash
+#rm -rf feeds/luci/themes/luci-theme-argon
+#rm -rf feeds/luci/applications/luci-app-adguardhome
+#rm -rf feeds/packages/net/adguardhome
+#rm -rf feeds/luci/applications/luci-app-openclash
 
-git clone --depth=1 https://github.com/kenzok8/small-package.git kenzok8-packages
-cp -rf kenzok8-packages/ddnsto package/ddnsto
-cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
-cp -rf kenzok8-packages/wrtbwmon package/wrtbwmon
-cp -rf kenzok8-packages/luci-app-wrtbwmon package/luci-app-wrtbwmon
-cp -rf kenzok8-packages/adguardhome package/adguardhome
-cp -rf kenzok8-packages/luci-app-adguardhome package/luci-app-adguardhome
-rm -rf kenzok8-packages
+#git clone --depth=1 https://github.com/kenzok8/small-package.git kenzok8-packages
+#cp -rf kenzok8-packages/ddnsto package/ddnsto
+#cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
+#cp -rf kenzok8-packages/wrtbwmon package/wrtbwmon
+#cp -rf kenzok8-packages/luci-app-wrtbwmon package/luci-app-wrtbwmon
+#cp -rf kenzok8-packages/adguardhome package/adguardhome
+#cp -rf kenzok8-packages/luci-app-adguardhome package/luci-app-adguardhome
+#rm -rf kenzok8-packages
 
-git clone --depth=1 https://github.com/vernesong/OpenClash.git OpenClash-packages
-cp -rf OpenClash-packages/luci-app-openclash package/luci-app-openclash
-rm -rf OpenClash-packages
+#git clone --depth=1 https://github.com/vernesong/OpenClash.git OpenClash-packages
+#cp -rf OpenClash-packages/luci-app-openclash package/luci-app-openclash
+#rm -rf OpenClash-packages
 
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
-./scripts/feeds update -a
-./scripts/feeds install -a
+#./scripts/feeds update -a
+#./scripts/feeds install -a
