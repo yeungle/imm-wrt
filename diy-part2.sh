e582881b0a1238881aacd2fb9e2d71f8ff99694a
yeungle <75871729+yeungle@users.noreply.github.com>
2026-06-04 11:40:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,18 +20,25 @@
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 #rm -rf feeds/luci/themes/luci-theme-argon
-#rm -rf feeds/luci/applications/luci-app-adguardhome
+rm -rf feeds/luci/applications/luci-app-adguardhome
 #rm -rf feeds/packages/net/adguardhome
 #rm -rf feeds/luci/applications/luci-app-openclash
 
-#git clone --depth=1 https://github.com/kenzok8/small-package.git kenzok8-packages
-#cp -rf kenzok8-packages/ddnsto package/ddnsto
-#cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
-#cp -rf kenzok8-packages/wrtbwmon package/wrtbwmon
-#cp -rf kenzok8-packages/luci-app-wrtbwmon package/luci-app-wrtbwmon
-#cp -rf kenzok8-packages/adguardhome package/adguardhome
-#cp -rf kenzok8-packages/luci-app-adguardhome package/luci-app-adguardhome
-#rm -rf kenzok8-packages
+git clone --depth=1 https://github.com/kenzok8/small-package.git kenzok8-packages
+cp -rf kenzok8-packages/ddnsto package/ddnsto
+cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
+cp -rf kenzok8-packages/wrtbwmon package/wrtbwmon
+cp -rf kenzok8-packages/luci-app-wrtbwmon package/luci-app-wrtbwmon
+cp -rf kenzok8-packages/adguardhome package/adguardhome
+cp -rf kenzok8-packages/luci-app-adguardhome package/luci-app-adguardhome
+cp -rf kenzok8-packages/fastnet package/fastnet
+cp -rf kenzok8-packages/luci-app-fastnet package/luci-app-fastnet
+cp -rf kenzok8-packages/luci-app-oaf package/luci-app-oaf
+cp -rf kenzok8-packages/oaf package/oaf
+cp -rf kenzok8-packages/open-app-filter package/open-app-filter
+rm -rf kenzok8-packages
+
+git clone --depth=1 -b openwrt-23.05 https://github.com/coolsnowwolf/luci package/luci-app-accesscontrol
 
 #git clone --depth=1 https://github.com/vernesong/OpenClash.git OpenClash-packages
 #cp -rf OpenClash-packages/luci-app-openclash package/luci-app-openclash
