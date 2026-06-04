b6affa41a70fa7b229f99d8417e6eb5554d72dfe
yeungle <75871729+yeungle@users.noreply.github.com>
2026-06-04 13:56:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,7 +21,7 @@
 
 #rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-adguardhome
-#rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/adguardhome
 #rm -rf feeds/luci/applications/luci-app-openclash
 
 git clone --depth=1 https://github.com/kenzok8/small-package.git kenzok8-packages
@@ -29,13 +29,13 @@ cp -rf kenzok8-packages/ddnsto package/ddnsto
 cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
 cp -rf kenzok8-packages/wrtbwmon package/wrtbwmon
 cp -rf kenzok8-packages/luci-app-wrtbwmon package/luci-app-wrtbwmon
-#cp -rf kenzok8-packages/adguardhome package/adguardhome
+cp -rf kenzok8-packages/adguardhome package/adguardhome
 cp -rf kenzok8-packages/luci-app-adguardhome package/luci-app-adguardhome
 cp -rf kenzok8-packages/fastnet package/fastnet
 cp -rf kenzok8-packages/luci-app-fastnet package/luci-app-fastnet
-cp -rf kenzok8-packages/luci-app-oaf package/luci-app-oaf
-cp -rf kenzok8-packages/oaf package/oaf
-cp -rf kenzok8-packages/open-app-filter package/open-app-filter
+#cp -rf kenzok8-packages/luci-app-oaf package/luci-app-oaf
+#cp -rf kenzok8-packages/oaf package/oaf
+#cp -rf kenzok8-packages/open-app-filter package/open-app-filter
 rm -rf kenzok8-packages
 
 #git clone --depth=1 https://github.com/vernesong/OpenClash.git OpenClash-packages
